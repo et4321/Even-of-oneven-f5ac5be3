c1234a475f419ced2ae94b1bef8540b978324f46
et4321 <54795466+et4321@users.noreply.github.com>
2019-10-02 11:17:25 +0200

    Uitwerking van de exercise

---
+++ even_of_oneven.php
@@ -0,0 +1,15 @@
+<?php
+echo "ik weet als eem getal even of oneven is kies een getal.\n";
+$a = readline ();
+
+if (($a % 2) == 1)
+  { echo "$a is oneven." ;}
+
+
+if (($a % 2) == 0)
+  { echo "$a is even." ;}
+
+
+
+
+?>
